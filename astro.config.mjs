6a462542334f165fe48061e1389b068243453f91
macairec <macairec@univ-grenoble-alpes.fr>
2024-06-02 23:32:12 +0200

    update website

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://github.com/macairececile.github.io',
+  site: 'https://cecile-macaire.github.io',
   integrations: [mdx(), sitemap(), tailwind()]
 });
